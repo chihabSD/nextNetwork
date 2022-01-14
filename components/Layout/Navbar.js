8623b0ac272692de59cfbcd9a86f80373e16c30e
chihabsd <adamchihab2012@hotmail.com>
2022-01-14 10:07:58 +0000

    latest update

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,27 +1,27 @@
 import React from "react";
-import Link from "next/link";
 import { Menu, Container, Icon } from "semantic-ui-react";
-
 import { useRouter } from "next/router";
+import Link from "next/link";
+
 function Navbar() {
   const router = useRouter();
 
-  // get the active route
-  const isActive = (route) => router.pathname === route;
+  const isActive = route => router.pathname === route;
+
   return (
     <Menu fluid borderless>
-      <Container>
+      <Container text>
         <Link href="/login">
           <Menu.Item header active={isActive("/login")}>
             <Icon size="large" name="sign in" />
             Login
           </Menu.Item>
         </Link>
+
         <Link href="/signup">
-          {/* <Menu.Item header active={true}> */}
           <Menu.Item header active={isActive("/signup")}>
             <Icon size="large" name="signup" />
-            Register
+            Signup
           </Menu.Item>
         </Link>
       </Container>
